d86636fd92ffb7715cb2a531ca248ddd16d29756
rafilaos <rafilaos@gmx.at>
2025-08-20 19:22:42 +0200

    routing
    with homepage, signin, login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {Homepage} from './homepage/homepage';
+import {Login} from './login/login';
+import {Signin} from './signin/signin';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "", component: Homepage},
+  {path: "login", component: Login},
+  {path: "signin", component: Signin},
+];
